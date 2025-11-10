37545753af068529c97917d5136f8a1ed885265e
Sulieman Alhassan <sulimanalhassan.123.as@gmail.com>
2025-11-10 02:40:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js (Version with Welcome Message)
+// index.js (PWA Enabled Server)
 const express = require('express');
 const http = require('http');
 const app = express();
@@ -8,79 +8,36 @@ const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
 // This is YOUR list of approved events.
-const approvedEvents = [
-    "coding Army's class",
-    "app creators class",
-    "never hide chat room privately",
-    "General Student Chat"
-];
+const approvedEvents = [ "coding Army's class", "app creators class", "never hide chat room privately", "General Student Chat" ];
+
+// --- PWA UPGRADE: This line is new/updated ---
+// This tells the server to allow access to static files like manifest.json, service-worker.js, and icon.png
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// The rest of the server code is exactly the same...
 const rooms = {};
-
 io.on('connection', (socket) => {
   socket.on('join room', ({ username, room }) => {
     if (approvedEvents.includes(room)) {
         socket.join(room);
         socket.username = username;
         socket.room = room;
-
-        if (!rooms[room]) {
-            rooms[room] = [];
-        }
+        if (!rooms[room]) { rooms[room] = []; }
         rooms[room].push({ id: socket.id, name: username, status: 'online' });
-
-        // --- NEW FEATURE: Private Welcome Message ---
-        // Send a private welcome message ONLY to the user who just connected.
         socket.emit('system message', { text: `Welcome, ${username}! You have joined the '${room}' event.`, time: new Date().toLocaleTimeString() });
-        
-        // Send a public message to EVERYONE ELSE in the room.
         socket.to(room).emit('system message', { text: `${username} has joined the event.`, time: new Date().toLocaleTimeString() });
-        
         io.to(room).emit('update users', rooms[room]);
-
     } else {
         socket.emit('join error', 'This event does not exist. Please check the event name.');
     }
   });
-
-  socket.on('typing', ({ room, username }) => {
-    if(rooms[room]) {
-        const user = rooms[room].find(u => u.name === username);
-        if (user) user.status = 'typing...';
-        io.to(room).emit('update users', rooms[room]);
-    }
-  });
-
-  socket.on('stop typing', ({ room, username }) => {
-    if(rooms[room]) {
-        const user = rooms[room].find(u => u.name === username);
-        if (user) user.status = 'online';
-        io.to(room).emit('update users', rooms[room]);
-    }
-  });
-
-  socket.on('chat message', ({ username, room, message }) => {
-    const timestamp = new Date().toLocaleTimeString();
-    io.to(room).emit('chat message', { username, message, time: timestamp });
-  });
-
-  socket.on('disconnect', () => {
-    if (socket.username && socket.room) {
-        const room = socket.room;
-        const username = socket.username;
-        if (rooms[room]) {
-            rooms[room] = rooms[room].filter(user => user.id !== socket.id);
-            io.to(room).emit('system message', { text: `${username} has left the event.`, time: new Date().toLocaleTimeString() });
-            io.to(room).emit('update users', rooms[room]);
-        }
-    }
-  });
+  socket.on('typing', ({ room, username }) => { if(rooms[room]) { const user = rooms[room].find(u => u.name === username); if (user) user.status = 'typing...'; io.to(room).emit('update users', rooms[room]); } });
+  socket.on('stop typing', ({ room, username }) => { if(rooms[room]) { const user = rooms[room].find(u => u.name === username); if (user) user.status = 'online'; io.to(room).emit('update users', rooms[room]); } });
+  socket.on('chat message', ({ username, room, message }) => { const timestamp = new Date().toLocaleTimeString(); io.to(room).emit('chat message', { username, message, time: timestamp }); });
+  socket.on('disconnect', () => { if (socket.username && socket.room) { const room = socket.room; const username = socket.username; if (rooms[room]) { rooms[room] = rooms[room].filter(user => user.id !== socket.id); io.to(room).emit('system message', { text: `${username} has left the event.`, time: new Date().toLocaleTimeString() }); io.to(room).emit('update users', rooms[room]); } } });
 });
-
-server.listen(PORT, () => {
-  console.log(`Pro server with Welcome Message is running on port ${PORT}`);
-});
+server.listen(PORT, () => { console.log(`PWA server is running on port ${PORT}`); });
